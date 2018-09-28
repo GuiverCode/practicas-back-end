54c7a8ef465854bd68399a6660560d9078591e53
Guille <guille@localhost.localdomain>
2018-09-27 20:32:09 -0400

    primera practica

---
+++ src/main/java/com/guiver/webservice1/RestAppConfig.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.guiver.webservice1;
+
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+/**
+ * Clase de configuracion Rest
+ * @author guille
+ */
+@javax.ws.rs.ApplicationPath("webresources")//base URI
+public class RestAppConfig extends Application {
+
+    //Get a set of root resource and provider classes.
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        resources.add(com.guiver.webservice1.DepartmentService.class);
+        return resources;
+    }
+}
